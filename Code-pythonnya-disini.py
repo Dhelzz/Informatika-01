703b460d40006203df95d388ada728e007401f42
Dhelzz Programming <57311772+Dhelzz@users.noreply.github.com>
2024-02-29 15:21:59 +0700

    Code-pythonnya-disini.py

--- Code-pythonnya-disini.py
+++ Code-pythonnya-disini.py
@@ -1,11 +1,13 @@
 print ('progam menghitung luas persegi dan keliling Lingkaran')
-print ('/n=====================================================')
+print ('=====================================================')
+print (' ')
 luas_persegi = float(input('masukan luas persegi : '))
 jari_jari = float(input('masukan jari² lingkaran : '))
 luas = luas_persegi * luas_persegi
 phi = 3.14
 keliling = 2 * phi * jari_jari
-print ('/n=====================================================')
+print ('=====================================================')
+print (' ')
 print ('hasil luas lingkaran adalah :' ,round(luas,2))
 print ('hasil keliling lingkaran adalah :' ,round(keliling,2))
 
